ac57d297b93b9e70024f07f84d0990bab8cca3df
Daler <macbook@DALER.local>
2023-07-30 01:20:21 +0500

    lesson6

--- index.js
+++ index.js
@@ -19,10 +19,23 @@ const saveToken = async (token) => {
     printError(error.message)
   }
 }
+const getForcast = async () => {
+  try {
+    const response = await getWeather(process.env.CITY ?? 'uzbekistan')
+    console.log(response);
+  } catch (error) {
+    if (error?.response?.status == 404) {
+      printError('City not found')
+    } else if (error?.response?.status == 401) {
+      printError('Invalid token')
+    } else {
+      printError(error.message)
+    }
+  }
 
+}
 const startCLI = () => {
   const args = getArgs(process.argv);
-  console.log(process.env);
   if (args.h) {
     //help
     printHelper();
@@ -36,7 +49,7 @@ const startCLI = () => {
     //token
     return saveToken(args.t)
   }
-  getWeather(process.env.CITY ?? 'uzbekistan')
+  getForcast()
 };
 
 startCLI();
